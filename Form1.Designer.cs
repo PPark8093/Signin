566ce7819f5223bbbe8acf6e6c57bd5c2d3b176b
PPark8093 <110334276+PPark8093@users.noreply.github.com>
2022-12-17 23:02:39 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Signin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SigninButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("한컴 고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "로그인 창";
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("한컴 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.id.Location = new System.Drawing.Point(46, 78);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(151, 24);
+            this.id.TabIndex = 1;
+            this.id.Text = "아이디";
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("한컴 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.password.Location = new System.Drawing.Point(46, 124);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(151, 24);
+            this.password.TabIndex = 2;
+            this.password.Text = "비밀번호";
+            // 
+            // SigninButton
+            // 
+            this.SigninButton.Font = new System.Drawing.Font("한컴 고딕", 15F, System.Drawing.FontStyle.Bold);
+            this.SigninButton.Location = new System.Drawing.Point(46, 244);
+            this.SigninButton.Name = "SigninButton";
+            this.SigninButton.Size = new System.Drawing.Size(151, 39);
+            this.SigninButton.TabIndex = 3;
+            this.SigninButton.Text = "로그인";
+            this.SigninButton.UseVisualStyleBackColor = true;
+            this.SigninButton.Click += new System.EventHandler(this.SigninButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 333);
+            this.Controls.Add(this.SigninButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button SigninButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Signin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SigninButton_Click(object sender, EventArgs e)
+        {
+            string userId = id.Text;
+            string userPassword = password.Text;
+
+            if(userId.Equals("닭은 왼발잡이다") && userPassword.Equals("ㄹㅇㅋㅋ")) {
+                MessageBox.Show("로그인에 성공했습니다", "로그인");
+            } else
+            {
+                MessageBox.Show("로그인에 실패하였습니다", "로그인");
+            }
+        }
+    }
+}
